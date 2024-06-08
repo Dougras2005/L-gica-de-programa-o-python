3bd4d776207564e5074545df1b521ac276ed42e2
unknown <borgesdougras@gmail.com>
2024-06-07 21:55:50 -0300

    criação de arquivos de exemplo estrutura condicional

---
+++ ex3.py
@@ -0,0 +1,9 @@
+#Estrutor condicional if
+idade = int(input('Digite a idade:'))
+
+if idade >= 100:
+    print('Muito VEIO!!!')
+else:
+    print('Novinho do veio')
+
+print('Viva ao Adiba Elias')
---
+++ ex4.py
@@ -0,0 +1,10 @@
+# Variáveis
+nota1 = float(input('digite a nota 1: '))
+nota2 = float(input('digite a nota 2: '))
+media = (nota1 + nota2) /2
+if media >= 6:
+    print('Aprovado')
+elif media >= 5:
+    print('recuperaçao')
+else:
+    print('Reprovado')
